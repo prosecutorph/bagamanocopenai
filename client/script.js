af62c5dccadebd9e8ff4f7e78cd307e61f46bcb5
Richard Vega <121314810+prosecutorph@users.noreply.github.com>
2023-01-14 02:39:24 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,4 +1,4 @@
-import bot from './assets/bot.svg'
+import bot from './assets/1bot.svg'
 import user from './assets/user.svg'
 
 const form = document.querySelector('form')
@@ -49,12 +49,11 @@ function chatStripe(isAi, value, uniqueId) {
         `
         <div class="wrapper ${isAi && 'ai'}">
             <div class="chat">
-/*                <div class="profile">
+                <div class="profile">
                     <img 
                       src=${isAi ? bot : user} 
                       alt="${isAi ? 'bot' : 'user'}" 
                     /> 
-*/
                 </div>
                 <div class="message" id=${uniqueId}>${value}</div>
             </div>
