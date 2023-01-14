7d74fd20a1056f10440723ac4424f79588eeccdb
Richard Vega <121314810+prosecutorph@users.noreply.github.com>
2023-01-14 20:18:15 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.get('/', async (req, res) => {
 app.get('/warmup', async (req, res) => {
   res.status(200).send({
     message: 'Server is awake'
-    console.log('Someone Send a Signal!'))
+    console.log('Someone Send a Signal!')
   })
 })
 
